0db44c4ef4d93e8043ce54cbbb6cd9b1f4097d6f
suley <suley@LAPTOP-VDM65UF4>
2025-01-20 21:18:51 +0300

    Add project files.

---
+++ AcunMedyaAkademiPortfolio/App_Start/RouteConfig.cs
@@ -0,0 +1,50 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+//using System.Web.Mvc;
+//using System.Web.Routing;
+
+//namespace AcunMedyaAkademiPortfolio
+//{
+//    public class RouteConfig
+//    {
+//        public static void RegisterRoutes(RouteCollection routes)
+//        {
+//            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+//            routes.MapRoute(
+//                name: "Default",
+//                url: "{controller}/{action}/{x}",
+//                defaults: new { controller = "Testimonial", action = "Index", id = UrlParameter.Optional }
+//            );
+//        }
+//    }
+//}
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace AcunMedyaAkademiPortfolio
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            // Varsayılan rotayı düzeltelim -*-- yüksek ihtimalle yaptığın bir değişiklik buraya da yansıdı, bu kodu chate attığımda x yerine id koydu bu kısımda neden x koydu yalan yok benim de fikrim yok :) 
+            // şu anda sorunsuz çalışıyor gözüküyor şurayı bir araştırmak lazım .Evet teşekkür ederim rica ederim iyi akşamlar.iyi akşamlar
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}", // {x} yerine {id} kullanıyoruz
+                defaults: new { controller = "Testimonial", action = "Index", id = UrlParameter.Optional } // id burada opsiyonel
+            );
+        }
+    }
+}
---
+++ AcunMedyaAkademiPortfolio/Controllers/DefaultController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AcunMedyaAkademiPortfolio.Models;
+namespace AcunMedyaAkademiPortfolio.Controllers
+{
+    public class DefaultController : Controller
+    {
+        DbPortfolioEntities db = new DbPortfolioEntities();
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();        
+        }
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialFeature()
+        {
+            var values = db.TblFeature.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialAbout()
+        {
+            var values = db.TblAbout.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialSkill()
+        {
+            var values = db.TblSkill.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialStatistic()
+        {
+            var skillcount = db.TblSkill.ToList().Count();
+            ViewBag.SkillCount = skillcount;
+            var Projectcount = db.TblProject.ToList().Count();
+            ViewBag.ProjectCount = Projectcount;
+            var TestimonialCount = db.TblTestimonial.ToList().Count();
+            ViewBag.TestimonialCount = TestimonialCount;
+            var ServiceCount = db.TblService.ToList().Count();
+            ViewBag.ServiceCount = ServiceCount;
+            return PartialView();
+        }
+        public PartialViewResult PartialServices()
+        {
+            var values = db.TblService.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialContact()
+        {
+            var values = db.TblContact.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialBanner()
+        {
+            var values = db.TblBanner.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialFooter()
+        {
+            var values = db.TblContact.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialProject()
+        {
+            var values = db.TblProject.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialTestimonial()
+        {
+            var values = db.TblTestimonial.ToList();
+            return PartialView(values);
+        }
+    }
+}
